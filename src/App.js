aa063988ae481607f958b0a9e6a650d69e622a04
mahmoud <opssheesh@gmail.com>
2021-09-27 11:14:46 +0800

    completed the applet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { Search } from "./components/search/search.component.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => this.setState({ monsters: data }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    let monsters = this.state.monsters;
+    let search = this.state.searchField;
+    let cards = monsters.filter((e) =>
+      e.name.toLowerCase().includes(search.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1> Robots Rolodex</h1>
+        <Search handleChange={this.handleChange} place={"Search Robots"} />
+        <CardList cards={cards} search={this.state.searchField} />
+      </div>
+    );
+  }
 }
 
 export default App;
